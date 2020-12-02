9d3489e1e3d0e3644820066420f1e044b9f17069
Heltzel <heltzel.service@hotmail.com>
2020-12-02 23:01:51 +0100

    Installing electron-reload (auto refresh frontend)

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { BrowserWindow, app } = require('electron')
+const path = require('path')
+
 function createwindow() {
   const win = new BrowserWindow({
     width: 1200,
@@ -12,5 +14,8 @@ function createwindow() {
   })
   win.loadFile('./index.html')
 }
-
+// path to nodeModules/bin/electron.ps1
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+})
 app.whenReady().then(createwindow)
--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function () {
-  return <h1>Hello from React component</h1>
+  return (
+    <>
+      <h1>Hello from React components</h1>
+      <h2>Hello from electron-reload</h2>
+    </>
+  )
 }
